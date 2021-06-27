adf8107847ebafb60b52b7c2f2b4f7cdc6077cd2
edafs <edafs@protonmail.com>
2021-06-27 19:49:27 -0400

    card adapter from api response to our catalog db

---
+++ LegacyDeckBuilder/LegacyDeckBuilder/Models/Adapter/CardAdapter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LegacyDeckBuilder.Models.Data;
+using LegacyDeckBuilder.Models.Response;
+
+namespace LegacyDeckBuilder.Models.Adapter
+{
+	public static class CardAdapter
+	{
+		/// <summary>
+		///		Converts a list of <see cref="CardInfo"/> cards from the YGODB api
+		///		to a list of <see cref="CardCatalog"/> of our data type.
+		/// </summary>
+		public static List<CardCatalog> ToData(this IEnumerable<CardInfo> cards)
+		{
+			if(cards.Count() == 0)
+			{
+				return new List<CardCatalog>();
+			}
+
+			List<CardCatalog> convertedCards = new List<CardCatalog>();
+
+			foreach(CardInfo card in cards)
+			{
+				CardCatalog convertedCard = new CardCatalog()
+				{
+					CardId = card.Id,
+					CardName = card.Name,
+					ImageUrl = card.Images.FirstOrDefault().FullImage
+				};
+
+				convertedCards.Add(convertedCard);
+			}
+
+			return convertedCards;
+		}
+	}
+}
